b143b1b78e9a4a86bf143a72bd11dfd2b7c6862f
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-26 00:26:43 +0530

    Changes regarding Module Demo

--- ModuleDemo/AnotherModule.py
+++ ModuleDemo/AnotherModule.py
@@ -6,3 +6,15 @@ def method():
 if __name__ == '__main__':
     method()
 
+def add():
+    print("Inside add() method.")
+
+def sub():
+    print("Inside sub() method.")
+
+def main():
+    add()
+    sub()
+
+main()
+
--- ModuleDemo/MainModule.py
+++ ModuleDemo/MainModule.py
@@ -7,7 +7,23 @@ am.method()
 
 def fun1():
     print("from fun1")
+    am.add()
 
 def fun2():
     print("from fun2")
+    am.sub()
 
+def main():
+    fun1()
+    fun2()
+
+main()
+
+count = 0
+def recursive_fun():
+    global count
+    count += 1
+    print("Hello World! Count:", count)
+#    recursive_fun() # Recursive call is currently commented out as it will result in Stack Overflow.
+
+recursive_fun()
