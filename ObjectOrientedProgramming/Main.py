4de5a27ff47d8b14ecd7393efdc1ff5896bbd8a6
Arnab Banerjee <arnab.ban09@gmail.com>
2019-12-28 21:39:09 +0530

    Added new tutorials about classes and objects - types of methods.

--- ObjectOrientedProgramming/Main.py
+++ ObjectOrientedProgramming/Main.py
@@ -207,6 +207,128 @@ print("Car2 Wheels:", car2.wheels, "ID of Car2.Wheels:", id(car2.wheels))
 print("---------------------------------------------------------------------------------------------------------------")
 # Types of Methods
 # 3 types of methods - Instance methods, Class methods, and Static methods.
+# In Instance methods, we have 2 different types of methods - Accessor methods and Mutator methods.
+# Accessor methods - to only fetch the value(s).
+# Mutator methods - to modify the value(s).
+# See below for all the demonstrations.
+class Student:
+    # Here is a class or static variable - school.
+    school = "Dum Dum Krishna Kumar Hindu Academy, Motijheel, Dum Dum, Kolkata, West Bengal (WB), India"
+    school_location = "Motijheel, Dum Dum, Kolkata, West Bengal (WB), India"
+    school_pincode = "700074"
+    school_city = "Kolkata, West Bengal (WB), India"
+
+    # Here is the constructor having marks_maths, marks_physics, and marks_chemistry as its parameters,
+    # and is creating three new instance variables - marks_maths, marks_physics, and marks_chemistry and assigning
+    # them from the passed parameters.
+    def __init__(self, name, standard, marks_maths, marks_physics, marks_chemistry):
+        self.name = name
+        self.standard = standard
+        self.marks_maths = marks_maths
+        self.marks_physics = marks_physics
+        self.marks_chemistry = marks_chemistry
+
+    # This is an Instance method, specifically an Accessor method or Getter.
+    def get_name(self):
+        return self.name
+
+    # This is an Instance method, specifically an Accessor method or Getter.
+    def get_standard(self):
+        return self.standard
+
+    # This is an Instance method, specifically an Accessor method or Getter.
+    def get_marks_maths(self):
+        return self.marks_maths
+
+    # This is an Instance method, specifically an Accessor method or Getter.
+    def get_marks_physics(self):
+        return self.marks_physics
+
+    # This is an Instance method, specifically an Accessor method or Getter.
+    def get_marks_chemistry(self):
+        return self.marks_chemistry
+
+    # This is an Instance methods, specifically a Mutator method or Setter.
+    def set_name(self, name):
+        self.name = name
+
+    # This is an Instance methods, specifically a Mutator method or Setter.
+    def set_standard(self, standard):
+        self.standard = standard
+
+    # This is an Instance methods, specifically a Mutator method or Setter.
+    def set_marks_maths(self, marks_maths):
+        self.marks_maths = marks_maths
+
+    # This is an Instance methods, specifically a Mutator method or Setter.
+    def set_marks_physics(self, marks_physics):
+        self.marks_physics = marks_physics
+
+    # This is an Instance methods, specifically a Mutator method or Setter.
+    def set_marks_chemistry(self, marks_chemistry):
+        self.marks_chemistry = marks_chemistry
+
+    # Define a method to get the average of the three marks defined above in the constructor.
+    # This is an instance method, specifically an Accessor method as it is not modifying any data.
+    def average(self):
+        return (self.marks_maths + self.marks_physics + self.marks_chemistry) / 3
+
+    # Now we will be creating and using a Class method. As we have already declared a Class or Static variable
+    # - school above, we will be creating this Class method to access that class variable.
+    # To create a Class method, you need to pass a 'cls' as a parameter name, and that should be the first paramater.
+    # As for Instance methods, we have seen the use of 'self', for Class methods, 'cls' is similar to 'self', but the
+    # method doesn't belong to any instance, rather to a class. And that is why we are passing 'cls' to notify that
+    # this is a Class method. And 'cls' is fixed, already decided by the language, so you cannot use any other value
+    # to represent a class.
+    # Also in similar way as we have used any Instance variable with self - self.marks_maths, we will use the
+    # Class variable with cls - cls.school_city
+    # Another thing - to denote it as Class method, we will use a concept called Decorators. Decorators are similar to
+    # Java Annotations - @classmethod
+    @classmethod # This is the Decorator.
+    def general_info(cls):
+        print("School Name:", cls.school)
+        print("School Location:", cls.school_location)
+        print("School Pincode:", cls.school_pincode)
+        print("School City:", cls.school_city)
+
+    # This is just an Instance method. But from this instance method, we are calling the Class method.
+    # Look at the code below. Here as every Student object will be having different names, different standards,
+    # and different marks, but they will be having the same school with same details. So the School details are
+    # mentioned as Class variable and are accessed through Class method - general_info(). So we will will be calling
+    # the individual info methods for each of the objects created from Student class, and from the info() Instance
+    # method, we will be calling the general_info() Class method by class name - Student.general_info().
+    def info(self):
+        print("Name:", self.name, "\tStandard:", self.standard)
+        Student.general_info()
+
+    # Now the third one - The Static methods.
+    # Here, in case of Instance methods, we have passed 'self', in case of Class methods, we have passed - 'cls'.
+    # But in case of Static methods, we will not be passing anything. Rather we will be using another Decorator -
+    # @ staticmethod
+    @staticmethod # This is a Decorator
+    def class_info(): # Look at this - without 'self' or 'cls'
+        print("__name__", __name__)
+        print("__class__", __class__)
+
+# Creating 2 Student objects
+student_one = Student("Arnab Banerjee", "XII", 98.4, 96.3, 96.2)
+student_two = Student("Sourav Ganguly", "XI", 88.95, 80.22, 90.61)
+
+# Just printing the average marks for student_one and student_two
+print("Average of 1st Student:", student_one.average())
+print("Average of 2nd Student:", student_two.average())
+
+# Calling the info() Instance methods for both student_one and student_two
+# See comments above info() method in the class definition for further information.
+# This will also call the Class method - general_info().
+student_one.info()
+student_two.info()
+
+# This will call the Static method - class_info of class Student.
+# See the comments above of this method in the class definition.
+student_one.class_info()
+student_two.class_info()
+
 
 
 
