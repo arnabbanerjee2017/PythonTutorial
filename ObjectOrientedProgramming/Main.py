ac436dce9de525d1b7def6f5bed54d0b58da45a9
Arnab Banerjee <arnab.ban09@gmail.com>
2019-12-28 20:12:38 +0530

    Added new tutorials about classes and objects - types of variables.

--- ObjectOrientedProgramming/Main.py
+++ ObjectOrientedProgramming/Main.py
@@ -123,3 +123,91 @@ else:
 
 # Here we are calling the compare method of one object and passing the second object as parameter.
 print(comp1.compare(comp2))
+
+# ---------------------------------------------------------------------------------------------------------------------
+print("---------------------------------------------------------------------------------------------------------------")
+# Types of variables
+# 2 types of variables - Instance Variables and Class or Static Variables
+class Car:
+    # Below variable - wheels is a class or static variable and belongs to the Class Namespace.
+    # As this is a class variable, this is shared across the objects.
+    wheels = 4      # A Class or Static Variable
+
+    def __init__(self):
+        # Below variables are instance variables. Means they are bound to a particular instance.
+        # Every time you create an object, that object will be created in a separate space in the heap memory, though
+        # they have same values. So below, car1 and car2 will be having same value and at the time of creation,
+        # they will be identical objects, still they will be created separately having different id or address.
+        # But in the object, as they are identical, they are having same values. So both of the objects will contain
+        # the below variables having same values. In this case, as Python is memory optimized, the variables will point
+        # to same memory locations. Like in car1, the brand is 'TATA', and in car2, the brand is also 'TATA'. So,
+        # actually in memory, a single 'TATA' object will be created and both brand of car1 ad car2 will point to
+        # that same object.
+        self.brand = "TATA"     # An Instance variable
+        self.model = "TIAGO"    # An Instance variable
+        self.engine = "Twin Valve 1.2L Turbo Petrol"    # An Instance variable
+        self.mil = "12"         # An Instance variable
+
+    # This is a print method just to demonstrate the the instance and class variables.
+    def print(self):
+        print("Brand:", self.brand, "\tModel:", self.model, "\tEngine:", self.engine, "\tMileage:", self.mil)
+        print("Wheels:", Car.wheels, "ID of Wheels:", id(Car.wheels))
+
+# Here we are creating 2 Car objects - car1 and car2
+car1 = Car()
+car2 = Car()
+
+# Here we are calling the print() method just to show the values after the object is created.
+car1.print()
+car2.print()
+
+# Here we are making some changes to the car1 objects.
+car1.model = "TIGOR"
+car1.mil = 8
+
+print("<<<<<<After Change>>>>>>")
+
+# Here we are again calling the print() method of the objects to check whether the above changes made are
+# reflected or not
+car1.print()
+car2.print()
+
+print("<<<<<<After Change>>>>>>")
+
+# Here we are trying to change the class variable through an object.
+# This will not change the static or class variable, rather this creates a new wheels instance
+# variable belonging to car1 object.
+car1.wheels = 6
+
+# Here you can see the print statements below. Make a note of it.
+# First when the print() method of car1 is called, you can see the values as it was earlier.
+# Also note that address of the wheels variable - the class variable. This is shared by all the objects of that class.
+# Now, we have just tried to modify the class variable through the car1 object. See above.
+# But in this case, this attempt just created another instance variable - wheels in the car1 objects and this didn't
+# make any changes to the actual class variable. See the id or the address of the class variable and the car1.wheels
+# variable. In case of car1, the id of wheels and car1.wheels are different. But in case of car2, both are same.
+car1.print()
+print("Car1 Wheels:", car1.wheels, "ID of Car1.Wheels:", id(car1.wheels))
+car2.print()
+print("Car2 Wheels:", car2.wheels, "ID of Car2.Wheels:", id(car2.wheels))
+
+print("<<<<<<After Change>>>>>>")
+
+# Now we are trying to change the class variable through class name.
+Car.wheels = 6
+
+# Here below, check that the the wheels is now a static/class variable and the change is reflected to all te objects.
+# Compare the id or address of the wheels objects across the objects - car1 and car2, and Car class.
+car1.print()
+print("Car1 Wheels:", car1.wheels, "ID of Car1.Wheels:", id(car1.wheels))
+car2.print()
+print("Car2 Wheels:", car2.wheels, "ID of Car2.Wheels:", id(car2.wheels))
+
+# ---------------------------------------------------------------------------------------------------------------------
+print("---------------------------------------------------------------------------------------------------------------")
+# Types of Methods
+# 3 types of methods - Instance methods, Class methods, and Static methods.
+
+
+
+
